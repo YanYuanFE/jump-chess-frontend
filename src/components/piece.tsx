fd1c2cc6984f52f068b30bf93a2d8416fb34e22f
YanYuan <yan950414@gmail.com>
2024-12-30 14:37:04 +0800

    fix: lint

--- src/components/piece.tsx
+++ src/components/piece.tsx
@@ -6,15 +6,16 @@ interface PieceProps {
 }
 
 export function Piece({ player, onClick }: PieceProps) {
+  console.log(player, 'pp');
   return (
-    <div 
+    <div
       className={`w-12 h-12 rounded-full flex items-center justify-center cursor-pointer
-        ${player === 'X' ? 'bg-blue-500 text-white' : 
-          player === 'O' ? 'bg-red-500 text-white' : 'bg-gray-200'}`}
+        ${
+          player === 'GREEN' ? 'bg-blue-500 text-white' : player === 'ORANGE' ? 'bg-red-500 text-white' : 'bg-gray-200'
+        }`}
       onClick={onClick}
     >
       {player}
     </div>
   );
 }
-
